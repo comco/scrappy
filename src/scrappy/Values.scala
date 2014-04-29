997f14137ce366b8ae12dab13fe274a602059a14
Krasimir Georgiev <void.unsigned@gmail.com>
2014-04-30 01:17:39 +0300

    Added cases for evaluation of values.

--- src/scrappy/Values.scala
+++ src/scrappy/Values.scala
@@ -16,5 +16,32 @@ object Values extends Domain {
   case class SeqData(val datatype: SeqType, val elements: Seq[Data])
     extends Data
   
-  def select(pick: Picker, data: Data): Data = ???
+  def select(pick: Picker, data: Data): Data = tie(pick, data) match {
+    // basic
+  	case IdTie(data) => data
+    case AppTie(pick, data) => select(pick.second, select(pick.first, data))
+    // primitive
+    case ValTie(pick, _) => PrimitiveData(pick.v)
+    case FuncTie(pick, data) => PrimitiveData(pick.f(data.data))
+    case FoldTie(pick, _, elems) => PrimitiveData(pick.f(elems.map(_.data)))
+    case UnfoldTie(pick, data) => SeqData(pick.targetType, 
+        pick.f(data.data).map(PrimitiveData(_)))
+    // sequence
+    case MapTie(pick, data) => SeqData(pick.targetType, 
+        data.elements.map(select(pick.f, _)))
+    case FilterTie(pick, data) => ???
+    case SortTie(pick, data) => ???
+    case GroupTie(pick, data) => ???
+    case JoinTie(pick, data) => ???
+    case ZipTie(pick, data) => ???
+    // projections
+    case CoordinateTie(pick, data) => data.coordinates(pick.position)
+    case FeatureTie(pick, data) => data.features(pick.name)
+    case ElementTie(pick, data) => data.elements(pick.index)
+    // constructors
+    case TupleTie(pick, data) => ???
+    case StructTie(pick, data) => ???
+    case RepeatTie(pick, data) => SeqData(pick.targetType, 
+        Stream.continually(data))
+  }
 } 
