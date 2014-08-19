8f69af93e4de28da08b1ab05de763a94bfd21a49
Krasimir Georgiev <void.unsigned@gmail.com>
2014-08-19 23:38:50 +0300

    Finished Domain tests.

--- src/main/scala/com/github/comco/scrappy/Domain.scala
+++ src/main/scala/com/github/comco/scrappy/Domain.scala
@@ -2,74 +2,121 @@ package com.github.comco.scrappy
 
 import scala.language.higherKinds
 
+/**
+ * Representation of a polymorphic hierarchy of data with a type, supporting
+ * object algebraic polymorphism.
+ */
 trait Domain {
-  type Data <: BaseData
-  type PrimitiveData[T] <: BasePrimitiveData[T]
-  type TupleData <: BaseTupleData
-  type StructData <: BaseStructData
-  type SeqData <: BaseSeqData
   
+  /**
+   * Specific base class for data in the domain.
+   */
+  type Data <: BaseData
+  
+  /**
+   * Specific class for data of primitive type.
+   */
+  type PrimitiveData[T] <: BasePrimitiveData[T]
+  
+  /**
+   * Specific class for data of tuple type.
+   */
+  type TupleData <: BaseTupleData
+  
+  /**
+   * Specific class for data of struct type.
+   */
+  type StructData <: BaseStructData
+  
+  /**
+   * Specific class for data of seq type.
+   */
+  type SeqData <: BaseSeqData
+
+  /**
+   * Base class with general properties of all data, independent of domain.
+   */
   abstract class BaseData {
     def datatype: Type
   }
-  
+
+  /**
+   * Base mixin for primitive data.
+   */
   trait BasePrimitiveData[T] extends BaseData {
     this: PrimitiveData[T] =>
-      
+
     def datatype: PrimitiveType[T]
     def value: T
   }
-  
+
+  /**
+   * Base mixin for tuple data.
+   */
   trait BaseTupleData extends BaseData {
     this: TupleData =>
-      
+
     def datatype: TupleType
     def coordinates: IndexedSeq[Data]
-    
+
     def size: Int = datatype.size
-    
+
     def hasCoordinate(position: Int): Boolean = {
       datatype.hasCoordinate(position) && coordinates(position) != null
     }
-    
+
     def coordinate(position: Int): Data = {
-      require(datatype.hasCoordinate(position), s"Coordinate position: $position is out of bounds for TupleType: $datatype")
-      require(coordinates(position) != null, s"TupleData doesn't contain a coordinate at position: $position")
+      require(datatype.hasCoordinate(position),
+        s"Coordinate position: $position is out of bounds for TupleType: $datatype")
+      require(coordinates(position) != null,
+        s"TupleData doesn't contain a coordinate at position: $position")
+
       coordinates(position)
     }
   }
-  
+
+  /**
+   * Base mixin for struct data.
+   */
   trait BaseStructData extends BaseData {
     this: StructData =>
-      
+
     def datatype: StructType
     def features: Map[String, Data]
-    
+
     def hasFeature(name: String): Boolean = {
       features.contains(name) && features(name) != null
     }
+    
     def feature(name: String): Data = {
       require(hasFeature(name), s"StructData doesn't contain a feature named: $name")
+      
       features(name)
     }
   }
-  
+
+  /**
+   * Base mixin for seq data.
+   */
   trait BaseSeqData extends BaseData {
     this: SeqData =>
-      
+
     def datatype: SeqType
     def elements: Seq[Data]
-    
+
     def hasElement(index: Int): Boolean = {
       0 <= index && index < length && elements(index) != null
     }
-    
+
     def element(index: Int): Data = {
-      require(0 <= index && index < length, s"Index: $index is out of bounds for SeqData with length: $length.")
-      require(elements(index) != null, s"SeqData doesn't contain an element at index: $index")
+      require(0 <= index && index < length,
+        s"Index: $index is out of bounds for SeqData with length: $length.")
+      require(elements(index) != null,
+        s"SeqData doesn't contain an element at index: $index")
+
       elements(index)
     }
-    
+
     def length: Int = elements.length
   }
 }
--- src/test/scala/com/github/comco/scrappy/DomainSpec.scala
+++ src/test/scala/com/github/comco/scrappy/DomainSpec.scala
@@ -12,31 +12,33 @@ object TestDomain extends Domain {
 
   case class TupleData(val datatype: TupleType, val coordinates: IndexedSeq[Data])
     extends Data with BaseTupleData
-  
+
   case class StructData(val datatype: StructType, val features: Map[String, Data])
     extends Data with BaseStructData
-    
+
   case class SeqData(val datatype: SeqType, val elements: Seq[Data])
     extends Data with BaseSeqData
 }
 
 class DomainSpec extends FlatSpec {
   import TestDomain._
-  
+
   val pi = PrimitiveData[Int](3)
   val ps = PrimitiveData[String]("hi")
   val pb = PrimitiveData[Boolean](true)
-  
+
   "A PrimitiveData" should "have the right datatype" in {
-    Seq(pi, ps, pb).map(_.datatype) shouldEqual Seq(IntPrimitiveType, StringPrimitiveType, BooleanPrimitiveType)
+    Seq(pi, ps, pb).map(_.datatype) shouldEqual
+      Seq(IntPrimitiveType, StringPrimitiveType, BooleanPrimitiveType)
   }
-  
+
   it should "have have the right data" in {
     Seq(pi, ps, pb).map(_.value) shouldEqual Seq(3, "hi", true)
   }
-  
+
   val tt = TupleType(IntPrimitiveType, StringPrimitiveType)
   val tp = TupleData(tt, IndexedSeq(pi, ps))
+  val nt = TupleData(tt, IndexedSeq(pi, null))
   
   "A TupleData" should "have a TupleType datatype" in {
     tp.datatype shouldEqual tt
@@ -45,10 +47,81 @@ class DomainSpec extends FlatSpec {
   it should "have size" in {
     tp.size shouldEqual 2
   }
-  
-  it should "support getting coordinates out of it" in {
+
+  it should "support checking valid positions" in {
     (0 to 2).map(tt.hasCoordinate(_)) shouldEqual Seq(true, true, false)
-    val nullTuple = TupleData(tt, IndexedSeq(pi, null))
-    nullTuple.hasCoordinate(1) shouldEqual false
+    nt.hasCoordinate(1) shouldEqual false
+  }
+
+  it should "support extracting coordinates one by one" in {
+    (0 until 2).map(tp.coordinate(_)) shouldEqual Seq(pi, ps)
+  }
+  
+  it should "support extracting a sequence of the coordinates" in {
+    tp.coordinates shouldEqual Seq(pi, ps)
+  }
+  
+  it should "throw an exception when an invalid position is given" in {
+    an[IllegalArgumentException] should be thrownBy tp.coordinate(-1)
+    an[IllegalArgumentException] should be thrownBy nt.coordinate(1)
+  }
+  
+  val st = StructType("name", "a" -> IntPrimitiveType, "b" -> StringPrimitiveType)
+  val struct = StructData(st, Map("a" -> PrimitiveData(3), "b" -> PrimitiveData("ala")))
+  val nullStruct = StructData(st, Map("a" -> PrimitiveData(5), "b" -> null))
+  
+  
+  "A StructData" should "have a StructType datatype" in {
+    struct.datatype shouldEqual st
+  }
+  
+  it should "support checking feature names" in {
+    Seq("a", "b", null, "none") map (struct.hasFeature(_)) shouldEqual
+      Seq(true, true, false, false)
+  }
+  
+  it should "support extracting features by a valid name" in {
+    Seq("a", "b") map (struct.feature(_)) shouldEqual
+      Seq(PrimitiveData(3), PrimitiveData("ala"))
+  }
+  
+  it should "throw exception when an invalid feature name is given" in {
+    an[IllegalArgumentException] should be thrownBy struct.feature(null)
+    an[IllegalArgumentException] should be thrownBy struct.feature("none")
+    an[IllegalArgumentException] should be thrownBy nullStruct.feature("b")
+  }
+  
+  val seqType = SeqType(IntPrimitiveType)
+  val elems = Seq(PrimitiveData(3), PrimitiveData(4))
+  val seqValue = SeqData(seqType, elems)
+  val nullSeqValue = SeqData(seqType, Seq(PrimitiveData(5), null, PrimitiveData(6)))
+  
+  "A SeqData" should "have a SeqType datatype" in {
+    seqValue.datatype shouldEqual seqType
+  }
+  
+  it should "support extracting a sequence of all the elements" in {
+    seqValue.elements shouldEqual elems
+  }
+  
+  it should "have the right length" in {
+    seqValue.length shouldEqual 2
+  }
+  
+  it should "allow checking for elements" in {
+    Seq(0, 1, -1, 2) map(seqValue.hasElement(_)) shouldEqual
+      Seq(true, true, false, false)
+    
+    nullSeqValue.hasElement(1) shouldEqual false
+  }
+  
+  it should "support extracting elements by a valid index" in {
+    seqValue.element(0) shouldEqual PrimitiveData(3)
+    seqValue.element(1) shouldEqual PrimitiveData(4)
+  }
+  
+  it should "throw an exception when invalid index is given" in {
+    an[IllegalArgumentException] should be thrownBy seqValue.element(-1)
+    an[IllegalArgumentException] should be thrownBy nullSeqValue.element(1)
   }
 }
