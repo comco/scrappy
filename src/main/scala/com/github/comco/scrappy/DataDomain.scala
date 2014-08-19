ac9049374c9ef58e4a252452a29b71ce612354ad
Krasimir Georgiev <void.unsigned@gmail.com>
2014-08-20 00:44:32 +0300

    Added tests for DataDomain.

--- src/main/scala/com/github/comco/scrappy/DataDomain.scala
+++ src/main/scala/com/github/comco/scrappy/DataDomain.scala
@@ -1,5 +1,7 @@
 package com.github.comco.scrappy
 
+import scala.language.implicitConversions
+
 /**
  * Domain for bare data values.
  */
@@ -16,33 +18,73 @@ object DataDomain extends Domain {
       OriginatedDataDomain.PrimitiveData(this, origin)
   }
 
+  object PrimitiveData {
+    implicit def raw2PrimitiveData[T](value: T)(
+        implicit datatype: PrimitiveType[T]): PrimitiveData[T] = {
+      PrimitiveData(value)
+    }
+  }
+
   case class TupleData(val datatype: TupleType,
     val coordinates: IndexedSeq[Data])
       extends Data with BaseTupleData {
     require(coordinates != null, "Can't construct TupleData with null coordinates.")
-    require(datatype.size == coordinates.size, 
-        s"Invalid size of coordinates to construct a TupleData; expected: ${datatype.size}, actual: ${coordinates.size}.")
+    require(datatype.size == coordinates.size,
+      s"Invalid size of coordinates to construct a TupleData; expected: ${datatype.size}, actual: ${coordinates.size}.")
     require(datatype.coordinateTypes.zip(coordinates).forall {
       case (datatype, data) => data.datatype == datatype
     }, "Data coordinates types don't match tuple datatypes for TupleData construction.")
-        
+
     def originatedFrom(origin: Origin): OriginatedDataDomain.TupleData =
       OriginatedDataDomain.OriginalTupleData(this, origin)
+  }
+
+  object TupleData {
+    def apply(datatype: TupleType)(coordinates: Data*): TupleData = {
+      TupleData(datatype, coordinates.toIndexedSeq)
+    }
   }
 
   case class StructData(val datatype: StructType,
     val features: Map[String, Data])
       extends Data with BaseStructData {
-
+    require(features != null, "StructData cannot be constructed with null features")
+    require(features.keys.forall(datatype.hasFeature(_)), "Invalid feature name")
+    require(features.forall {
+      case (name, data) if data != null => data.datatype == datatype.featureType(name)
+      case _ => true
+    }, "Invalid feature type")
+    
     def originatedFrom(origin: Origin): OriginatedDataDomain.StructData =
       OriginatedDataDomain.OriginalStructData(this, origin)
+  }
+  
+  object StructData {
+    def apply(datatype: StructType)(features: (String, Data)*): StructData = {
+      StructData(datatype, features.toMap)
+    }
   }
 
   case class SeqData(val datatype: SeqType,
     val elements: Seq[Data])
       extends Data with BaseSeqData {
-
+    require(elements != null, "SeqData cannot be constructed with null elements")
+    require(elements.forall {
+      case element if element != null => element.datatype == datatype.elementType
+      case _ => true
+    }, "An element has invalid datatype")
+    
     def originatedFrom(origin: Origin): OriginatedDataDomain.SeqData =
       OriginatedDataDomain.OriginalSeqData(this, origin)
+  }
+  
+  object SeqData {
+    case class Dummy(private[SeqData] val real: SeqData)
+    
+    def apply(datatype: SeqType)(elements: Data*): Dummy = {
+      Dummy(SeqData(datatype, elements))
+    }
+    
+    implicit def Dummy2SeqData(dummy: Dummy): SeqData = dummy.real
   }
 }
--- src/test/scala/com/github/comco/scrappy/DataDomainSpec.scala
+++ src/test/scala/com/github/comco/scrappy/DataDomainSpec.scala
@@ -1,5 +1,80 @@
 package com.github.comco.scrappy
 
-class DataDomainSpec {
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers._
 
+import DataDomain._
+import PrimitiveType._
+import PrimitiveData.raw2PrimitiveData
+    
+class DataDomainSpec extends FlatSpec {
+  val a = PrimitiveData(3)
+  val b = PrimitiveData("hi")
+  val c = PrimitiveData(false)
+  
+  "A DataDomain.PrimitiveData" should "have the right value" in {
+    a.value shouldEqual 3
+    b.value shouldEqual "hi"
+    c.value shouldEqual false
+  }
+  
+  it should "have the right datatype" in {
+    a.datatype shouldEqual IntPrimitiveType
+    b.datatype shouldEqual StringPrimitiveType
+    c.datatype shouldEqual BooleanPrimitiveType
+  }
+  
+  it should "implicitly convert raw values" in {   
+    3.datatype shouldEqual IntPrimitiveType
+    3.value shouldEqual 3
+  }
+  
+  val tupleType = TupleType(IntPrimitiveType, StringPrimitiveType, BooleanPrimitiveType)
+  val tuple = TupleData(tupleType)(3, "hi", false)
+  
+  "A DataDomain.TupleData" should "provide the right coordinates" in {
+    tuple.coordinates shouldEqual
+      Seq(PrimitiveData(3), PrimitiveData("hi"), PrimitiveData(false))
+  }
+  
+  it should "validate the number of coordinates during construction" in {
+    an[IllegalArgumentException] should be thrownBy TupleData(tupleType)()
+    an[IllegalArgumentException] should be thrownBy TupleData(tupleType)(3, "hi")
+  }
+  
+  it should "validate the types of coordinates during construction" in {
+    an[IllegalArgumentException] should be thrownBy TupleData(tupleType)(3, "hi", "bye")
+  }
+  
+  val structType = StructType("struct", "a" -> IntPrimitiveType, "b" -> tupleType)
+  val struct = StructData(structType)("a" -> 3, "b" -> tuple)
+  
+  "A DataDomain.StructData" should "have the right features" in {
+    struct.features shouldEqual Map("a" -> PrimitiveData(3), "b" -> tuple)
+  }
+  
+  it should "validate against constructing with null features" in {
+    an[IllegalArgumentException] should be thrownBy struct.feature(null)
+  }
+  
+  it should "validate against constructing with uncompatible features" in {
+    an[IllegalArgumentException] should be thrownBy
+      StructData(structType)("a" -> 3, "b" -> 4)
+  }
+  
+  it should "validate against wrong feature names" in {
+    an[IllegalArgumentException] should be thrownBy
+      StructData(structType)("a" -> 4, "none" -> "hi")
+  }
+  
+  val seqType = SeqType(IntPrimitiveType)
+  val seq = SeqData(seqType)(1, 2, 3)
+  
+  "A DataDomain.SeqType" should "have the right elements" in {
+    seq.elements shouldEqual Seq(PrimitiveData(1), PrimitiveData(2), PrimitiveData(3))
+  }
+  
+  it should "validate for elements with wrong datatype" in {
+    an[IllegalArgumentException] should be thrownBy SeqData(seqType)("hi")
+  }
 }
