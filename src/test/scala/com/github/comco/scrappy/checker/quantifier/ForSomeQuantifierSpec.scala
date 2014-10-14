34eebffb1e2b0e9d88cd18d8c4020f9b671affba
"Krasimir Georgiev" <void.unsigned@gmail.com>
2014-10-14 18:54:23 +0300

    Added ForSomeQuantifier.

---
+++ src/test/scala/com/github/comco/scrappy/checker/quantifier/ForSomeQuantifierSpec.scala
@@ -0,0 +1,10 @@
+package com.github.comco.scrappy.checker.quantifier
+
+import org.scalatest.FlatSpec
+import com.github.comco.scrappy.CustomMatchers
+
+class ForSomeQuantifierSpec extends FlatSpec with CustomMatchers {
+  "A ForSomeQuantifier" should "be bad when empty" in {
+    
+  }
+}
