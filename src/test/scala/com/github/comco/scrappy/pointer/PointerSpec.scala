46ffa925cee8c2c8b9b94d682d2f064339300f1f
"Krasimir Georgiev" <void.unsigned@gmail.com>
2014-10-21 17:13:20 +0300

    Added Pointer concat and lca logic.

--- src/test/scala/com/github/comco/scrappy/pointer/PointerSpec.scala
+++ src/test/scala/com/github/comco/scrappy/pointer/PointerSpec.scala
@@ -8,7 +8,9 @@ import com.github.comco.scrappy.PrimitiveType.IntPrimitiveType
 import com.github.comco.scrappy.PrimitiveType.StringPrimitiveType
 import com.github.comco.scrappy.StructType
 import com.github.comco.scrappy.TupleType
-import com.github.comco.scrappy.picker.{SelfPicker, AndThenPicker, CoordinatePicker}
+import com.github.comco.scrappy.picker.AndThenPicker
+import com.github.comco.scrappy.picker.CoordinatePicker
+import com.github.comco.scrappy.picker.SelfPicker
 
 class PointerSpec extends FlatSpec with CustomMatchers {
   "A SelfPointer" should "have the right targetType" in {
@@ -35,5 +37,43 @@ class PointerSpec extends FlatSpec with CustomMatchers {
   it should "have the right composite picker" in {
     StepPointer(SelfPointer(tupleType), CoordinateStep(tupleType, 1)).picker shouldEqual
       AndThenPicker(SelfPicker(tupleType), CoordinatePicker(tupleType, 1))
+  }
+  
+  def tt(i: Int): TupleType = {
+    if (i == 0) TupleType(IntPrimitiveType, StringPrimitiveType)
+    else TupleType(IntPrimitiveType, StringPrimitiveType, tt(i - 1))
+  }
+  
+  "A Pointer" should "support concat" in {
+    SelfPointer(tt(4)).concat(SelfPointer(tt(4))) shouldEqual SelfPointer(tt(4))
+    val pt2 = SelfPointer(tt(4)).append(CoordinateStep(tt(4), 1))
+    SelfPointer(tt(4)).concat(pt2) shouldEqual pt2
+    pt2.concat(SelfPointer(StringPrimitiveType)) shouldEqual pt2
+    val pt3 = SelfPointer(tt(4)).append(CoordinateStep(tt(4), 2))
+    val pt4 = SelfPointer(tt(3)).append(CoordinateStep(tt(3), 2))
+    val pt5 = pt3.append(CoordinateStep(tt(3), 2))
+    pt3.concat(pt4) shouldEqual pt5
+  }
+  
+  it should "validate pointer types during concat" in {
+    val pt1 = SelfPointer(tt(0)).append(CoordinateStep(tt(0), 0))
+    itShouldBeDisallowed calling pt1.concat(pt1)
+  }
+  
+  it should "support longestCommonAncestor" in {
+    val pt1 = SelfPointer(tt(4)).append(CoordinateStep(tt(4), 2)).append(CoordinateStep(tt(3), 2)).append(CoordinateStep(tt(2), 1))
+    val pt2 = SelfPointer(tt(4)).append(CoordinateStep(tt(4), 2)).append(CoordinateStep(tt(3), 1))
+    val pt3 = SelfPointer(tt(4)).append(CoordinateStep(tt(4), 2))
+    pt1.longestCommonAncestor(pt2) shouldEqual pt3
+    itShouldBeDisallowed calling pt1.longestCommonAncestor(SelfPointer(tt(3)))
+  }
+  
+  it should "provide steps" in {
+    val pt1 = SelfPointer(tt(4))
+    pt1.steps shouldEqual List.empty
+    val st1 = CoordinateStep(tt(4), 2)
+    val st2 = CoordinateStep(tt(3), 1)
+    val pt2 = pt1.append(st1).append(st2)
+    pt2.steps shouldEqual List(st1, st2)
   }
 }
